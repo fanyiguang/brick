ea0592bb643c264d72da4addcd3f4eb4f80b3f4f
fanyiguan <2397711850@qq.com>
2024-12-06 11:49:08 +0800

    chore: add safe goroutine

---
+++ Go/Go.go
@@ -0,0 +1,37 @@
+package Go
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"runtime"
+	"strings"
+)
+
+var writer io.Writer = os.Stderr
+
+func SetWriter(w io.Writer) {
+	writer = w
+}
+
+func Go(fun func()) {
+	go func(f func()) {
+		defer func() {
+			if err := recover(); err != nil {
+				Recover(err)
+			}
+		}()
+
+		f()
+	}(fun)
+}
+
+func Recover(err interface{}) {
+	callers := make([]uintptr, 15)
+	_ = runtime.Callers(3, callers)
+	for k, _ := range callers {
+		frame, _ := runtime.CallersFrames(callers[k : k+1]).Next()
+		_, _ = writer.Write([]byte(fmt.Sprintf("runtime.CallersFrames failed, err: %v file: %v line: %v func: %v\n", err, frame.File, frame.Line, frame.Function)))
+	}
+	_, _ = writer.Write([]byte(strings.Repeat("-", 50) + "\n"))
+}
